3b622b45a0593e5487a2aa885086d33fabd7037f
Ankit Kumar <ankiitkumar07@gmail.com>
2020-07-14 11:29:07 +0530

    app name change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bookstore'));
+app.use(express.static(__dirname + '/dist/bookstore-app-client'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/bookstore/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bookstore-app-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
